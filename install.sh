99e2eef8fcccaf834552d43ae6e4a2dc457d327b
Alex Beltran <abeltran2@ucmerced.edu>
2016-02-11 17:49:14 -0800

    Transition from i3status to i3blocks.

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 ln -s ~/git/dotfiles/bin ~/
 ln -s ~/git/dotfiles/config/i3/ ~/.config/
 ln -s ~/git/dotfiles/config/i3status/ ~/.config/
+ln -s ~/git/dotfiles/config/i3blocks/i3blocks.conf ~/.i3blocks.conf
+ln -s ~/git/dotfiles/config/i3blocks/.i3blocks ~/
 
